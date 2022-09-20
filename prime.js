0120cc9044383619d49a1c571080d8495f1acb52
subhakanta panda <meunknown466@gmail.com>
2022-09-20 23:43:09 +0530

    i am right the code

--- prime.js
+++ prime.js
@@ -1,3 +1,26 @@
-for(var i =0;i<N;i++){
-    console.log(N%i)
-}
+function primecheck(N){
+    let count =0;
+    
+    for(let i=1; i<=N; i++)
+    {
+        
+            if(N%i==0)
+            {
+                count++
+            }
+        }
+    
+        if(count==2)
+        {
+        console.log(N, "is Prime");
+        }
+        else
+        {
+        console.log(N, "is not Prime");
+        }
+    
+    }
+        
+    primecheck(13)
+    
+    
