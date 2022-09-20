4d9a3bf7a574c87997d061b06c7d5c0fb6e086d5
Subhransu Pradhan <dynamicwarrior007@gmail.com>
2022-09-20 23:17:24 +0530

    changed the code

--- prime.js
+++ prime.js
@@ -1,3 +1,23 @@
-for(var i =0;i<N;i++){
-    console.log(N%i)
+function prime(N){
+for(let i=1; i<=N; i++)
+{
+	let count=0;
+	for(let j=1; j<=i; j++)
+	{
+		if(i%j==0)
+		{
+			count++;
+		}
+	}
+	if(count==2)
+	{
+	console.log(i, "is Prime");
+	}
+	else
+	{
+	console.log(i, "is not Prime");
+	}
 }
+}	
+
+prime(10);
